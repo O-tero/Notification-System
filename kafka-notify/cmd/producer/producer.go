ff1075be2a962824fc201f9e5727a9186e69d404
Otero <oteronickbrian@gmail.com>
2024-02-29 00:25:35 +0300

    setting up kafka consumer

--- kafka-notify/cmd/producer/producer.go
+++ kafka-notify/cmd/producer/producer.go
@@ -20,26 +20,27 @@ const (
     KafkaTopic         = "notifications"
 )
 
-// Helper functions
+// HELPER FUNCTIONS
 var ErrUserNotFoundInProducer = errors.New("user not found")
 
 func findUserByID(id int, users []models.User) (models.User, error) {
-	for _, user := range users {
-		if user.ID == id {
-			return user, nil
-		}
-	}
-	return models.User{}, ErrUserNotFoundInProducer
+    for _, user := range users {
+        if user.ID == id {
+            return user, nil
+        }
+    }
+    return models.User{}, ErrUserNotFoundInProducer
 }
 
-func getIDFromRequest(formValue string, ctx *gin.Context) (int.error) {
-	id, err := strconv.Atoi(ctx.PostForm(formValue))
-	if err != nil {
-		return 0, fmt.Errorf(
-			"failed to parse ID from form value %s: %w", formValue, err)
-	}
-	return id, nil 
+func getIDFromRequest(formValue string, ctx *gin.Context) (int, error) {
+    id, err := strconv.Atoi(ctx.PostForm(formValue))
+    if err != nil {
+        return 0, fmt.Errorf(
+            "failed to parse ID from form value %s: %w", formValue, err)
+    }
+    return id, nil
 }
+
 // KAFKA RELATED FUNCTIONS 
 func sendKafkaMessage(producer sarama.SyncProducer,
     users []models.User, ctx *gin.Context, fromID, toID int) error {
